11f14323a259697924e2e9d364e38dc527d44232
Lerskk <faustoleo02@gmail.com>
2022-10-04 19:20:16 -0300

    fix comments and learn about constants

--- practice0/init.rkt
+++ practice0/init.rkt
@@ -1,29 +1,29 @@
 #lang racket/base
 (require 2htdp/image)
 
-#; Numbers
+; Numbers
 
-#;; 12 * 5 - 7 * 6
+;; 12 * 5 - 7 * 6
 (- (* 12 5) (* 7 6))
 
-#;; 3 * 5 - 7 * 4 / 14 + 3 / 1
+;; 3 * 5 - 7 * 4 / 14 + 3 / 1
 (- (* 3 5) (+ (/ (* 7 4) 14) (/ 3 1)))
 
-#;; cos(0.8) + 1/5 + sen(0.5) * 3.5
+;; cos(0.8) + 1/5 + sen(0.5) * 3.5
 (+ (cos 0.8) (/ 1 5) (* (sin 0.5) 3.5))
 
-#; Strings
+; Strings
 (string-append "Hi" ", " "I'm DrRacket")
 
 (string-length "How long could it be")
 (string-append "The string has: " (number->string (string-length (string-append "How long could it be" ". " "And now?"))) " " "characters")
 (substring "How is DrRacket doing?" 4 6)
-#;; (substring "?" 4 6) ERROR: valid range [0, 1]
+;; (substring "?" 4 6) ERROR: valid range [0, 1]
 
 
 #| Bools
-  True is represented by #true or #t
-  False is represented by #false or #f
+  True is represented by true or #t
+  False is represented by false or #f
 
   There are some operators
     - and p q
@@ -45,13 +45,25 @@
 (and (= 3 3) #false)
 (and (= 3 3) (< 3 777))
 
-#; Images
+; Images
 (circle 20 "solid" "green")
 
-(overlay (rectangle 20 20 "solid" "blue") (circle 7 "solid" "green")) #;; creates a shape as product of multiple shapes, the stacking order is left is in front of right
+;; creates a shape as product of multiple shapes, the stacking order is left is in front of right
+(overlay (rectangle 20 20 "solid" "blue") (circle 7 "solid" "green")) 
 
-(empty-scene 100 100) #;; create an empty scene limeted by the size that you put in. It will be represented by balck lines
+;; create an empty scene limeted by the size that you put in. It will be represented by balck lines
+(empty-scene 100 100) 
 
-(place-image (circle 10 "solid" "blue") 40 80 (empty-scene 100 100) #;; put an image on a set of coordinates
+;; put an image on a set of coordinates
+(place-image (circle 10 "solid" "blue") 40 80 (empty-scene 100 100)) 
 
-(+ (image-width (circle 10 "solid" "red")) (image-height (rectangle 10 20"solid" "blue"))) #;; mesure the width of an image
+;; mesure the width of an image
+(+ (image-width (circle 10 "solid" "red")) (image-height (rectangle 10 20"solid" "blue"))) 
+
+; Constant
+
+(define ABC "abc")
+(define P "Pluto")
+ABC
+P
+(string-append "abc + pluto: " ABC P)
