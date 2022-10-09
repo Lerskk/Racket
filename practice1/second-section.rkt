466ffd11f0b1fc5258a8bb8789e1b7adc0d4e27f
Lerskk <faustoleo02@gmail.com>
2022-10-09 12:08:08 -0300

    second section finished

--- practice1/second-section.rkt
+++ practice1/second-section.rkt
@@ -18,9 +18,38 @@
 
 (define (clasificar t) (cond [(< t 0) "Muy frío (MF)"]
                              [(and (> t 0) (< t 15)) "Frío (F)"]
-                             [(and (>= t 15) (< t 25)) "Agradable (A)"]
+                             [(and (> t 15) (< t 25)) "Agradable (A)"]
                              [(> t 25) "Caluroso (C)"]))
 
 (clasificar -3)
 (clasificar 28)
 (clasificar 15)
+
+(define (sgn2 x) (cond [(< x 0) -1]
+                       [(= x 0) 0]
+                       [(> x 0) 1]))
+
+(define (sgn3 x) 
+	(cond 
+		[(and (image? x) (string=? (classify-image x) "Square")) 0]
+		[(and (image? x) (or (string=? (classify-image x) "Narrow") (string=? (classify-image x) "Very narrow"))) -1]
+		[(image? x) 1]
+		[(and (boolean? x) x) 1]
+		[(boolean? x) 0]
+		[(number? x) (sgn2 x)]
+		[(and (string? x) (number? (string->number x))) (sgn2 (string->number x))]
+		[(string? x) "Couldn't convert string->number"]
+		[else "Class not supported"]))
+
+(sgn3 #t)
+(sgn3 #f)
+(sgn3 45)
+(sgn3 -45)
+(sgn3 0)
+(sgn3 (rectangle 3 3 "solid" "red"))
+(sgn3 (rectangle 1 3 "solid" "red"))
+(sgn3 (rectangle 2 3 "solid" "red"))
+(sgn3 (rectangle 3 1 "solid" "red"))
+(sgn3 (rectangle 3 2 "solid" "red"))
+(sgn3 "hola")
+(sgn3 (cons 1 2))
