4b23f41c650a0282a3ddde80ba2454fa45492479
Lerskk <faustoleo02@gmail.com>
2022-10-12 15:58:41 -0300

    exercice 4 done practice 3

--- practice3/init.rkt
+++ practice3/init.rkt
@@ -55,8 +55,33 @@
 		[(number? (string->number new-size)) (* (string->number new-size) 10)]
 		[else key-state]))
 
-(big-bang INITIAL-SIZE
-	[to-draw make-circle]
-	[on-tick re-size 0.05]
-	[on-key change-size]
-	[stop-when stop?])
+;;; (big-bang INITIAL-SIZE
+;;; 	[to-draw make-circle]
+;;; 	[on-tick re-size 0.05]
+;;; 	[on-key change-size]
+;;; 	[stop-when stop?])
+
+(define CANVAS-HIGHT 300)
+(define CANVAS-WIDTH 100)
+(define DELTA 10)
+
+(define (move-circle pos)
+	(place-image (circle 20 "solid" "turquoise") 50 pos (empty-scene CANVAS-WIDTH CANVAS-HIGHT)))
+
+(define (move state direction)
+	(cond
+		[(and (key=? direction "up") (>= (- state DELTA) 20)) (- state DELTA)]
+		[(and (key=? direction "down") (<= (+ state DELTA) (- CANVAS-HIGHT 20))) (+ state DELTA)]
+		[(key=? direction " ") (/ CANVAS-HIGHT 2)]
+		[else state]))
+
+(define (mouse-handler state x y mouse-event)
+	(cond
+		[(and (string=? mouse-event "drag") (>= (- y DELTA) 20) ) y]
+		[(and (string=? mouse-event "drag") (<= (+ y DELTA) (- CANVAS-HIGHT 20))) y]
+		[else state]))
+
+(big-bang (/ CANVAS-HIGHT 2)
+	[to-draw move-circle]
+	[on-key move]
+	[on-mouse mouse-handler])
