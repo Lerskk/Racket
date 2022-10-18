beefeb2e42518bba8151b83c77427fdf5ae32417
Lerskk <faustoleo02@gmail.com>
2022-10-18 20:29:06 -0300

    text editor v0.2 change data structure to controle the content color and size of the text

--- practice3/terminal.rkt
+++ practice3/terminal.rkt
@@ -2,15 +2,33 @@
 (require 2htdp/image)
 (require 2htdp/universe)
 
-(define (draw state)
-	(place-image/align (text state 20 "indigo") 0 0 "left" "top" (empty-scene 800 60))
+(define-struct Terminal [txt font-size font-color])
+(define START (make-Terminal "" 12 "black"))
+
+(define (draw terminal-state)
+	(place-image/align (text (Terminal-txt terminal-state) (Terminal-font-size terminal-state) (Terminal-font-color terminal-state)) 0 0 "left" "top" (empty-scene 800 60))
 )
 
-(define (keys-manager state key)
-	(cond
-		[(and (not (string=? state "")) (key=? key "\b")) (substring state 0 (- (string-length state) 1))]
-		[else (string-append state key)]))
+(define (select-color key)
+	(cond 	
+		[(key=? key "f1") "red"]
+		[(key=? key "f2") "turquoise"]
+		[(key=? key "f3") "yellow"]
+		[(key=? key "f4") "green"]
+		[(key=? key "f5") "white"]
+		[(key=? key "f6") "black"]
+		[else #f]))
 
-(big-bang ""
+(define (keys-manager terminal-state key)
+	(cond
+		[(select-color key) (make-Terminal (Terminal-txt terminal-state) (Terminal-font-size terminal-state) (select-color key))]
+		[(key=? key "up") (make-Terminal (Terminal-txt terminal-state) (+ (Terminal-font-size terminal-state) 2) (Terminal-font-color terminal-state))]
+		[(and (key=? key "down") (<= (Terminal-font-size terminal-state) 2)) (make-Terminal (Terminal-txt terminal-state) (Terminal-font-size terminal-state) (Terminal-font-color terminal-state))]
+		[(and (key=? key "down") (> (Terminal-font-size terminal-state) 2)) (make-Terminal (Terminal-txt terminal-state) (- (Terminal-font-size terminal-state) 2) (Terminal-font-color terminal-state))]
+		[(key=? key "\b") 
+			(make-Terminal (substring (Terminal-txt terminal-state) 0 (- (string-length (Terminal-txt terminal-state)) 1)) (Terminal-font-size terminal-state) (Terminal-font-color terminal-state))]
+		[else (make-Terminal (string-append (Terminal-txt terminal-state) key) (Terminal-font-size terminal-state) (Terminal-font-color terminal-state))]))
+;;; 
+(big-bang START
 	[to-draw draw]
 	[on-key keys-manager])
