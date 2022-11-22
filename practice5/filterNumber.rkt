f1dbcc4b1e9749a27c628fb2c6da02e10e3475a1
Lerskk <faustoleo02@gmail.com>
2022-11-22 18:46:53 -0300

    filter number with filter

--- practice5/filterNumber.rkt
+++ practice5/filterNumber.rkt
@@ -1,19 +1,19 @@
 #lang racket
 (require test-engine/racket-tests)
 
-;; a List-without-n is:
+;; a list-of-numbers is:
 ;; '()
-;; (cons Number List-without-n)
+;; (cons Number list-of-numbers)
 
-(check-expect (filter-n empty 2) empty)
-(check-expect (filter-n (list 1 2 3) 2) (list 1 3))
-(check-expect (filter-n (list 1 -2 3) 2) (list 1 -2 3))
-(check-expect (filter-n (list 0 0 0 0) 0) empty)
+;; data representation
+;; you take numbers and represent them as Numbers
 
-(define (filter-n list n)
-	(cond
-		[(empty? list) empty]
-		[(not (= (first list) n)) (cons (first list) (filter-n (rest list) n))]
-		[else (filter-n (rest list) n)]))
+;; signature and function purpose
+;; function list-of-numbers n -> list-of-numbers-without-n
+;; you take a list of numbers and a number and then return the list without n
+
+(check-expect (filter (lambda (number) (not (= number 2))) (list 1 2 3 4 5 2)) (list 1 3 4 5))
+(check-expect (filter (lambda (number) (not (= number 2))) (list 2)) empty)
+(check-expect (filter (lambda (number) (not (= number 2))) empty) empty)
 
 (test)
