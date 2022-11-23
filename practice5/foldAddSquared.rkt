9dff8c6f592f22ccb6426ade531c7be2153efb5c
Lerskk <faustoleo02@gmail.com>
2022-11-23 06:26:31 -0300

    add squared list

---
+++ practice5/foldAddSquared.rkt
@@ -0,0 +1,23 @@
+#lang racket
+(require test-engine/racket-tests)
+
+;; a list-of-numbers is:
+;; '()
+;; (cons Number list-of-numbers)
+;; is a list which his element are defined as Number
+
+;; data representation
+;; numbers are represented by numbers
+
+;; signature and function puprose
+;; list-of-numbers -> Number
+;; takes a list of numbers and returns the addition of their squares
+
+;; examples
+(check-expect (foldl (lambda (number sum) (+ sum (expt number 2))) 0 (list 2 3)) 13)
+(check-expect (foldl (lambda (number sum) (+ sum (expt number 2))) 0 (list -2 -3)) 13)
+(check-expect (foldl (lambda (number sum) (+ sum (expt number 2))) 0 (list 2 -2)) 8)
+(check-expect (foldl (lambda (number sum) (+ sum (expt number 2))) 0 (list 0 0)) 0)
+(check-expect (foldl (lambda (number sum) (+ sum (expt number 2))) 0 empty) 0)
+
+(test)
